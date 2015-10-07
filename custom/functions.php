1569c9d871e438abc4b0697b0e5e46ca98539dbe
SmashBrando <brandon.j.kidd@gmail.com>
2015-10-06 20:23:07 -0600

    initial working prototype

---
+++ custom/functions.php
@@ -0,0 +1,50 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * functions.php
+ * Add PHP snippets here
+ */
+
+/**
+ * Hide Ground Shipping when Customer is outside Colorado
+ *
+ * UPDATED FOR WOOCOMMERCE 2.1
+ *
+ */
+add_filter( 'woocommerce_package_rates', 'hide_out_of_state_ground_shipping' , 10, 2 );
+
+/**
+ *
+ * @param array $available_methods
+ */
+function hide_out_of_state_ground_shipping( $rates, $package ) {
+ 	
+ 	//states to exclude
+	$excluded_states = array( 'CO' );
+
+	//create an array for shipping classes
+	$shipping_classes = array();
+
+	//loop through cart to determine if a shipping classes are present and add them to the shipping_class array
+    if ( sizeof( WC()->cart->get_cart() ) > 0 ) {
+        foreach( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+             //print_r($cart_item);
+             $shipping_class = $cart_item['data']->get_shipping_class();
+             $shipping_classes[] = $shipping_class;
+		}
+	}
+
+	//check if a specific shipping class is in array
+	//print_r($shipping_classes);
+
+	//if Shipping Method exists and the State to exclude is not in the respective array and the Shipping class is in the respective array
+	if( isset( $rates['fedex:GROUND_HOME_DELIVERY'] ) AND !in_array( WC()->customer->shipping_state, $excluded_states) AND in_array('steaks', $shipping_classes ) ) :
+ 
+		// Unset the fedexshipping method
+		unset( $rates['fedex:GROUND_HOME_DELIVERY'] );
+
+	endif;
+
+	return $rates;
+}
---
+++ theme-customisations.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Plugin Name: Custom Shipping Restrictions for WooCommerce
+ * Description: A plugin to remove shipping options for some users based on specific conditions.
+ * Version: 	1.0.0
+ * Author: 		SweetWood
+ * Author URI: 	http://www.sweetwood.com/
+ *
+ * @package Woo_Ship_Restrict
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Main Woo_Ship_Restrict Class
+ *
+ * @class Woo_Ship_Restrict
+ * @version	1.0.0
+ * @since 1.0.0
+ * @package	Woo_Ship_Restrict
+ */
+final class Woo_Ship_Restrict {
+
+	public function __construct () {
+		add_action( 'init', array( $this, 'Woo_Ship_Restrict_setup' ), -1 );
+	}
+
+	/**
+	 * Setup all the things
+	 */
+	public function Woo_Ship_Restrict_setup() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'Woo_Ship_Restrict_css' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'Woo_Ship_Restrict_js' ) );
+
+		require_once( 'custom/functions.php' );
+	}
+
+	/**
+	 * Enqueue the CSS
+	 * @return void
+	 */
+	public function Woo_Ship_Restrict_css() {
+		wp_enqueue_style( 'custom-css', plugins_url( '/custom/style.css', __FILE__ ) );
+	}
+
+	/**
+	 * Enqueue the Javascript
+	 * @return void
+	 */
+	public function Woo_Ship_Restrict_js() {
+		wp_enqueue_script( 'custom-js', plugins_url( '/custom/custom.js', __FILE__ ), array( 'jquery' ) );
+	}
+
+} // End Class
+
+/**
+ * The 'main' function
+ * @return void
+ */
+function __Woo_Ship_Restrict_main() {
+	new Woo_Ship_Restrict();
+}
+
+/**
+ * Initialise the plugin
+ */
+add_action( 'plugins_loaded', '__Woo_Ship_Restrict_main' );
